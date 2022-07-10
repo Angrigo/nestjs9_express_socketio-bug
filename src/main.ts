c6cebd6aef6a6e557567490cae2ce37dd1c06ed4
J-Hoh <J-Hoh@users.noreply.github.com>
2022-07-10 19:29:01 +0200

    nest8_fastify3

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify"
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+	
+	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 	app.enableCors({
 		origin: "*"
 	});
-	await app.listen(3000)
-	console.log(`Application is running on: ${await app.getUrl()}`)
+	await app.listen(3000);
+	console.log(`Application is running on: ${await app.getUrl()}`);
+	
 }
 bootstrap()
